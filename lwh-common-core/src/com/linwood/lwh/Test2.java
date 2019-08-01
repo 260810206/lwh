48cd18585c9139acd6b237d8f3e2aa32a5c096bd
linwenhui9889 <linwenhui9889@163.com>
2019-08-02 00:50:18 +0800

    新增Test2類

--- lwh-common-core/src/com/linwood/lwh/Test2.java
+++ lwh-common-core/src/com/linwood/lwh/Test2.java
@@ -1,10 +1,10 @@
 package com.linwood.lwh;
 
-public class Test {
+public class Test2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(11);
+
 	}
 
 }
